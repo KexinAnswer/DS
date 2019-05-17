947e3723134c906a60603364cee840fc355332f5
KexinAnswer <925433202@qq.com>
2019-05-17 22:08:35 +0800

    修改冒泡排序

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 
+void swap(int* a, int* b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
 
 void BubbleSort(int arr[] , int size)
 {
   int i = 0;
   int j = 0;
-  int isSort = 0;
+  int isSort;
   for(i = 0 ; i<size; i++)
   {
-    for()
+    isSort = 0;
+    for(j = 0; j < size - 1 - i; j++)
+    {
+
+      if(arr[j] > arr[j+1])
+      {
+        swap(arr+j,arr+j+1);
+        isSort = 1;
+      }
+    
+    }
+    if(isSort == 0)
+      break;
+
   }
 }
-int main()
+
+void Print(int arr[],int size)
 {
-  int arr[] = {5,4,3,2,1};
+  int i;
+  for(i = 0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  } 
+  printf("\n");
+}   
+int  main()
+{   
+  int arr[] = {5,4,3,2,1,7,11,57,34,12};
   int size = sizeof(arr)/sizeof(int);
+    
+  BubbleSort(arr,size);
 
-
+  Print(arr,size);
   return 0;
 }
